c85515f2a5c1337c4497a36cc4e4fa14553a9135
Wescom1 <146420887+Wes-codelife@users.noreply.github.com>
2024-02-13 16:37:05 -0600

    Update main.js

--- 01 - JavaScript Drum Kit/js/main.js
+++ 01 - JavaScript Drum Kit/js/main.js
@@ -1,12 +1,16 @@
-window.addEventListener('keydown', function(e){
-    // console.log(e.keyCode)
+
+
+
+function playSound(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if(!audio) return;                                                      //if no keymap-stop function from running
     audio.currentTime = 0;                                              
     audio.play();
     key.classList.add('playing');
-});
+
+}
+
 
 function removeTransition(e){
     if(e.propertyName !== 'transform') return                      //skip it if its not a transform
@@ -15,3 +19,5 @@ function removeTransition(e){
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition ));
+
+window.addEventListener('keydown',playSound);
